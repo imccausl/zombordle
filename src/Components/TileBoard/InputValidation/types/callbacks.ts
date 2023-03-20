0478d6884131f89d414fb87ccabce53ca86f641f
Ian McCausland <ian.mccausland@tophat.com>
2023-03-20 19:04:50 -0400

    fix issue with types

--- src/Components/TileBoard/InputValidation/types/callbacks.ts
+++ src/Components/TileBoard/InputValidation/types/callbacks.ts
@@ -3,10 +3,10 @@ import type React from 'react'
 export type ValidateFn = (value: string) => string | undefined
 
 export type OnValidateSuccessCallback = (
-    e: React.FocusEvent<HTMLInputElement>,
+    e: React.FocusEvent<HTMLInputElement> | React.ChangeEvent<HTMLInputElement>,
 ) => void
 export type OnValidateErrorCallback = (
-    e: React.FocusEvent<HTMLInputElement>,
+    e: React.FocusEvent<HTMLInputElement> | React.ChangeEvent<HTMLInputElement>,
 ) => void
 
 export type TrackedFieldCallbacks = {
