7c0d0b83a006caf94406da6d2c00f4821f1f8254
Ian McCausland <imccausl@gmail.com>
2023-03-25 16:53:20 -0400

    add test boilerplate

---
+++ src/Components/TileBoard/InputValidation/FormProvider/FormProvider.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react'
+
+import { FormProvider, type FormProviderProps } from '.'
+
+describe('FormProvider', () => {
+    it.todo('keeps track of registered fields')
+})
