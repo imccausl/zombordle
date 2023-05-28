ca28304f6eb0d9c64764c884e0a44c91f6a1e3e8
Ian McCausland <imccausl@gmail.com>
2023-05-28 18:38:37 -0400

    fix: prevent infinite tooltip reposition on smaller screens

--- app/src/Components/Revealer/Revealer.styles.ts
+++ app/src/Components/Revealer/Revealer.styles.ts
@@ -21,4 +21,13 @@ export const RevealerContentContainer = styled.div<RevealerContentContainerProps
     top: calc(100% + 5px);
     ${({ $defaultPosition }) =>
         PositionCSS[$defaultPosition ?? 'bottom-center']};
+
+    /**
+    * temporary fix to infinite repositioning issue when screen width is equal to 
+    * the revealer content width. Feels a tiny bit hacky and may want to go about
+    * this a bit more dynamically.
+    */
+    @media screen and (max-device-width: 530px) {
+        width: 270px;
+    }
 `
