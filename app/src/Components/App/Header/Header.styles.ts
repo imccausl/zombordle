e2887fe5f73af0474fcabf3c7fc3699fa054e4ad
Ian McCausland <imccausl@gmail.com>
2023-05-28 21:10:02 -0400

    feat: add zombo theme

---
+++ app/src/Components/App/Header/Header.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const StyledHeader = styled.header`
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: space-between;
+    height: 65px;
+    width: 100%;
+    border-bottom: lightgrey solid 1px;
+    margin-bottom: 20px;
+`
+export const StyledIconButton = styled.button`
+    border: none;
+    background: transparent;
+    width: 40px;
+    height: 40px;
+    padding: 5px;
+    margin: 0 5px;
+`
