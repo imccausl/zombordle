d6daa7b9bbf38d69cc430954c7e7888db15b604e
Ian McCausland <imccausl@gmail.com>
2023-06-07 20:44:09 -0400

    feat: support word lists of 6 and 7 letters

--- app/src/hooks/useWordList/index.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo } from 'react'
-
-import { fiveLetterWords } from '../../wordlists/fiveLetterWords'
-
-export const useWordList = () => {
-    return useMemo(() => fiveLetterWords, [])
-}
