1a2df62c9ffb0451e742d38ce719bb079f07b4cf
Ian McCausland <ian.mccausland@tophat.com>
2023-06-12 14:34:12 -0400

    feat: add logrocket

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,7 +1,11 @@
+import LogRocket from 'logrocket'
+
 import { Layout } from '../Components/Layout'
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
+
+LogRocket.init('mlov0a/zombordle-production')
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
