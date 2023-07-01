b702bedd40031d671e1d404b79621a2f2b0abd3b
Ian McCausland <imccausl@gmail.com>
2023-07-01 16:13:28 -0400

    fix: console errors (#137)

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@zombordle/design-tokens/variables.dark.css'
 import '@zombordle/design-tokens/variables.light.css'
 import LogRocket from 'logrocket'
+import Head from 'next/head'
 
 import { Layout } from '../Components/Layout'
 import '../styles/globals.css'
@@ -13,8 +14,39 @@ if (process?.env?.NODE_ENV === 'production') {
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Layout data-theme="dark">
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Head>
+                <meta name="theme-color" content="#FFFFFF" />
+                <meta
+                    name="description"
+                    content="An accessible wordle-like thinger"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+            </Head>
+            <Layout data-theme="dark">
+                <Component {...pageProps} />
+            </Layout>
+        </>
     )
 }
