86fc86f99e90f06f1593b9f093d93f89eede6fbb
Ian McCausland <imccausl@gmail.com>
2023-07-02 08:45:34 -0400

    fix: some light theme color tweaks

--- packages/design-tokens/tokens/application/background/dark.theme.ts
+++ packages/design-tokens/tokens/application/background/dark.theme.ts
@@ -2,7 +2,7 @@ module.exports = {
     background: {
         base: { value: '{color.grey.600.value}' },
         correct: { value: '{color.green.200.value}' },
-        present: { value: '{color.orange.100.value}' },
+        present: { value: '{color.orange.200.value}' },
         absent: { value: '{color.grey.500.value}' },
         neutral: { value: '{color.grey.100.value}' },
         invalid: { value: '{color.red.100.value}' },
