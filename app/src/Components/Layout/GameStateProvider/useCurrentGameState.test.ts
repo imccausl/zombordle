8383ef993bb2c6401c07c5f8e39676994e36b868
Ian McCausland <imccausl@gmail.com>
2023-07-15 20:31:52 -0400

    wip: add todo tests

---
+++ app/src/Components/Layout/GameStateProvider/useCurrentGameState.test.ts
@@ -0,0 +1,18 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import { useCurrentGameState } from './useCurrentGameState'
+
+describe('useCurrentGameState', () => {
+    it.todo('should return the game state for the current game')
+    it.todo('should return the initial game state if no game state is found')
+    it.todo('should set guesses for the current game')
+    it.todo('should set hasPlayed and lastPlayed values for the current game')
+    it.todo('should reset the current game state')
+    it.todo(
+        'should set hasPlayed, lastCompleted, and lastPlayed values for the current game',
+    )
+    it.todo('should return the current number of attempts for the current game')
+    it.todo('should return the hasPlayed value for the current game')
+    it.todo('should return the state of the guesses for the current game')
+    it.todo('should return the lastCompleted value for the current game')
+})
