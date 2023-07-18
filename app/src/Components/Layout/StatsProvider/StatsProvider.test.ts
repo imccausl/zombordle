41cfbcdd002f7f6f1d9867fb5c672f2f3f5d58cb
Ian McCausland <imccausl@gmail.com>
2023-07-18 08:14:01 -0400

    wip: add stats provider tests boilerplate

---
+++ app/src/Components/Layout/StatsProvider/StatsProvider.test.ts
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react'
+
+import { StatsProvider } from './StatsProvider'
+
+describe('StatsProvider', () => {})
---
+++ app/src/Components/Layout/StatsProvider/useCurrentStats.test.ts
@@ -0,0 +1,13 @@
+import { act, renderHook } from '@testling-library/react'
+
+import { useCurrentStats } from './useCurrentStats'
+
+describe('useCurrentStats', () => {
+    it.todo('should return the stats for the current word length')
+    it.todo('should set stats for the current word length')
+    it.todo('should return current streak for the current word lenght')
+    it.todo(
+        'should return correct guess distribution for the current word length',
+    )
+    it.todo('should return the max streak for the current word length')
+})
