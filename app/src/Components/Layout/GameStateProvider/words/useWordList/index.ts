215bc98f4fa52afd479ef520ed7549bd4a30ea1f
Ian McCausland <imccausl@gmail.com>
2023-07-21 18:37:41 -0400

    feat: add more words to the isValidWord validator (#182)

--- app/src/Components/Layout/GameStateProvider/words/useWordList/index.ts
+++ app/src/Components/Layout/GameStateProvider/words/useWordList/index.ts
@@ -2,6 +2,7 @@ import { useCallback, useMemo } from 'react'
 import seedrandom from 'seedrandom'
 
 import dictionary from '../dictionary.json'
+import validWords from '../validWords.json'
 
 const DEFAULT_LENGTH = 5
 const RANDOM_SEED = 'ohhai. this is a random seed.'
@@ -43,11 +44,12 @@ export const useWordList = (wordListLength: WordListLength = 5) => {
         [allowedLength, wordListLength],
     )
 
-    const isValidWord = useCallback((testString: string) => {
-        return Boolean(
-            (dictionary as Array<string>).includes(testString.toLowerCase()),
-        )
-    }, [])
+    const isValidWord = useCallback(
+        (testString: string) =>
+            (dictionary as Array<string>).includes(testString.toLowerCase()) ||
+            (validWords as Array<string>).includes(testString.toLowerCase()),
+        [],
+    )
 
     return useMemo(() => ({ wordList, isValidWord }), [wordList, isValidWord])
 }
