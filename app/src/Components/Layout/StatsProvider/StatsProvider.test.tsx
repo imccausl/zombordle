a061ea94c23df85b4ce1bdcd6a138eb08c97a083
Ian McCausland <imccausl@gmail.com>
2023-12-23 19:04:44 -0500

    wip: add more tests

--- app/src/Components/Layout/StatsProvider/StatsProvider.test.tsx
+++ app/src/Components/Layout/StatsProvider/StatsProvider.test.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import { GameStateProvider } from '../GameStateProvider'
 import * as useWordModule from '../GameStateProvider/words/useWord'
@@ -7,7 +7,7 @@ import { StatsProvider, useStats } from '.'
 
 const mockState = JSON.stringify({
     '5': {
-        guesses: ['trial', 'error', '5test'],
+        guesses: ['trial', 'error', 'smile', '5test'],
         hasPlayed: false,
         lastPlayed: null,
         lastCompleted: null,
@@ -34,9 +34,11 @@ describe('StatsProvider', () => {
 
     afterEach(() => {
         window.localStorage.clear()
+        vi.restoreAllMocks()
     })
 
-    it('should throw and error if context is missing', () => {
+    it('should throw an error if context is missing', () => {
+        vi.spyOn(console, 'error').mockImplementation(() => {})
         const ContextlessHookCall: React.FC = () => {
             const { maxStreak } = useStats()
 
@@ -46,19 +48,17 @@ describe('StatsProvider', () => {
         expect(() =>
             render(<ContextlessHookCall />),
         ).toThrowErrorMatchingInlineSnapshot(
-            '"useStats must be used within a StatsProvider"',
+            '[Error: useStats must be used within a StatsProvider]',
         )
     })
 
     describe('when user has guessed correctly (won)', () => {
-        vi.spyOn(useWordModule, 'useWord').mockReturnValue({
-            correctWord: 'trial',
-            wordList: ['error', '5test', 'trial'],
-            isValidWord: () => true,
-        })
-
-        afterEach(() => {
-            vi.restoreAllMocks()
+        beforeEach(() => {
+            vi.spyOn(useWordModule, 'useWord').mockReturnValue({
+                correctWord: 'trial',
+                wordList: ['error', '5test', 'trial'],
+                isValidWord: () => true,
+            })
         })
 
         it('should update the hasCompleted values in the game state', async () => {
@@ -77,8 +77,8 @@ describe('StatsProvider', () => {
             expect(gameState.lastPlayed).toBe(1688184000000)
         })
 
-        it.only('should increment the current streak value', () => {
-            const { rerender } = render(
+        it('should increment the current streak value', () => {
+            render(
                 <GameStateProvider>
                     <StatsProvider />
                 </GameStateProvider>,
@@ -94,6 +94,7 @@ describe('StatsProvider', () => {
             expect(statState.currentStreak).toBe(1)
 
             vi.setSystemTime(new Date(2023, 6, 2, 0, 0, 0, 0))
+
             window.localStorage.setItem(
                 'zombordle_gameState',
                 JSON.stringify({
@@ -102,28 +103,247 @@ describe('StatsProvider', () => {
                 }),
             )
 
-            // rerender(
-            //     <GameStateProvider>
-            //         <StatsProvider />
-            //     </GameStateProvider>,
-            // )
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
 
-            const nextStatState = JSON.parse(
+            expect(
+                JSON.parse(
+                    window.localStorage.getItem('zombordle_stats') as string,
+                )['5'].currentStreak,
+            ).toBe(2)
+        })
+
+        it('should increment the max streak value if current streak is greater than (previous) max streak', () => {
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            const statState = JSON.parse(
                 window.localStorage.getItem('zombordle_stats') as string,
             )['5']
-            expect(nextStatState.currentStreak).toBe(2)
+            const gameState = JSON.parse(
+                window.localStorage.getItem('zombordle_gameState') as string,
+            )
+
+            expect(statState.maxStreak).toBe(1)
+
+            vi.setSystemTime(new Date(2023, 6, 2, 0, 0, 0, 0))
+
+            window.localStorage.setItem(
+                'zombordle_gameState',
+                JSON.stringify({
+                    ...gameState,
+                    '5': { ...gameState['5'], hasPlayed: false },
+                }),
+            )
+
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            expect(
+                JSON.parse(
+                    window.localStorage.getItem('zombordle_stats') as string,
+                )['5'].maxStreak,
+            ).toBe(2)
         })
-        it.todo(
-            'should increment the max streak value if current streak is greater than (previous) max streak',
-        )
-        it.todo('should reset the the current streak to 1 if no successive win')
-        it.todo(
-            'should not increment the max streak value if current streak is less than (previous) max streak',
-        )
-        it.todo(
-            'should increment the number of attempts in the guess distribution',
-        )
+
+        it('should reset the the current streak to 1 if no successive win', () => {
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            const statState = JSON.parse(
+                window.localStorage.getItem('zombordle_stats') as string,
+            )['5']
+            const gameState = JSON.parse(
+                window.localStorage.getItem('zombordle_gameState') as string,
+            )
+
+            expect(statState.currentStreak).toBe(1)
+
+            vi.setSystemTime(new Date(2023, 6, 2, 0, 0, 0, 0))
+
+            window.localStorage.setItem(
+                'zombordle_gameState',
+                JSON.stringify({
+                    ...gameState,
+                    '5': { ...gameState['5'], hasPlayed: false },
+                }),
+            )
+
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            expect(
+                JSON.parse(
+                    window.localStorage.getItem('zombordle_stats') as string,
+                )['5'].currentStreak,
+            ).toBe(2)
+
+            vi.setSystemTime(new Date(2023, 6, 4, 0, 0, 0, 0))
+
+            window.localStorage.setItem(
+                'zombordle_gameState',
+                JSON.stringify({
+                    ...gameState,
+                    '5': {
+                        ...gameState['5'],
+                        lastCompleted: new Date(
+                            2023,
+                            6,
+                            2,
+                            0,
+                            0,
+                            0,
+                            0,
+                        ).getTime(),
+
+                        hasPlayed: false,
+                    },
+                }),
+            )
+
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            expect(
+                JSON.parse(
+                    window.localStorage.getItem('zombordle_stats') as string,
+                )['5'].currentStreak,
+            ).toBe(1)
+        })
+
+        it('should not increment the max streak value if current streak is less than (previous) max streak', () => {
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            const statState = JSON.parse(
+                window.localStorage.getItem('zombordle_stats') as string,
+            )['5']
+            const gameState = JSON.parse(
+                window.localStorage.getItem('zombordle_gameState') as string,
+            )
+
+            expect(statState.maxStreak).toBe(1)
+
+            vi.setSystemTime(new Date(2023, 6, 2, 0, 0, 0, 0))
+
+            window.localStorage.setItem(
+                'zombordle_gameState',
+                JSON.stringify({
+                    ...gameState,
+                    '5': { ...gameState['5'], hasPlayed: false },
+                }),
+            )
+
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            expect(
+                JSON.parse(
+                    window.localStorage.getItem('zombordle_stats') as string,
+                )['5'].maxStreak,
+            ).toBe(2)
+
+            vi.setSystemTime(new Date(2023, 6, 4, 0, 0, 0, 0))
+
+            window.localStorage.setItem(
+                'zombordle_gameState',
+                JSON.stringify({
+                    ...gameState,
+                    '5': {
+                        ...gameState['5'],
+                        lastCompleted: new Date(
+                            2023,
+                            6,
+                            2,
+                            0,
+                            0,
+                            0,
+                            0,
+                        ).getTime(),
+
+                        hasPlayed: false,
+                    },
+                }),
+            )
+
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            expect(
+                JSON.parse(
+                    window.localStorage.getItem('zombordle_stats') as string,
+                )['5'].maxStreak,
+            ).toBe(2)
+        })
+
+        it.only('should increment the number of attempts in the guess distribution', () => {
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            const statState = JSON.parse(
+                window.localStorage.getItem('zombordle_stats') as string,
+            )['5']
+            const gameState = JSON.parse(
+                window.localStorage.getItem('zombordle_gameState') as string,
+            )
+
+            expect(statState.distribution['4']).toBe(0)
+
+            vi.setSystemTime(new Date(2023, 6, 2, 0, 0, 0, 0))
+
+            window.localStorage.setItem(
+                'zombordle_gameState',
+                JSON.stringify({
+                    ...gameState,
+                    '5': { ...gameState['5'], hasPlayed: false },
+                }),
+            )
+
+            render(
+                <GameStateProvider>
+                    <StatsProvider />
+                </GameStateProvider>,
+            )
+
+            expect(
+                JSON.parse(
+                    window.localStorage.getItem('zombordle_stats') as string,
+                )['5'].distribution['4'],
+            ).toBe(1)
+        })
+
         it.todo('should set status to "win"')
+
         it.todo('should record the number of attempts it took to win')
     })
 
