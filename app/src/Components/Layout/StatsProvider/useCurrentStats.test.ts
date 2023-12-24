e183de1e92890a717b3455074fe23f076d1beac4
Ian McCausland <imccausl@gmail.com>
2023-12-24 14:18:18 -0500

    wip: replace todos in useCurrentStats tests

--- app/src/Components/Layout/StatsProvider/useCurrentStats.test.ts
+++ app/src/Components/Layout/StatsProvider/useCurrentStats.test.ts
@@ -1,13 +1,109 @@
-import { act, renderHook } from '@testling-library/react'
+import { act, renderHook } from '@testing-library/react'
 
 import { useCurrentStats } from './useCurrentStats'
 
-describe('useCurrentStats', () => {
-    it.todo('should return the stats for the current word length')
-    it.todo('should set stats for the current word length')
-    it.todo('should return current streak for the current word lenght')
-    it.todo(
-        'should return correct guess distribution for the current word length',
-    )
-    it.todo('should return the max streak for the current word length')
+const mockStatsObject = {
+    '5': {
+        attempts: 3,
+        status: 'win',
+        distribution: {
+            '1': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            loss: 10,
+        },
+        currentStreak: 2,
+        maxStreak: 9,
+    },
+    '6': {
+        attempts: 3,
+        status: 'loss',
+        distribution: {
+            '1': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            loss: 10,
+        },
+        currentStreak: 0,
+        maxStreak: 9,
+    },
+    '7': {
+        attempts: 3,
+        status: 'win',
+        distribution: {
+            '1': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            loss: 10,
+        },
+        currentStreak: 1,
+        maxStreak: 3,
+    },
+}
+
+const mockStats = JSON.stringify(mockStatsObject)
+
+describe.each([[5], [6], [7]] as const)('useCurrentStats', (wordLength) => {
+    beforeEach(() => {
+        window.localStorage.setItem('zombordle_stats', mockStats)
+    })
+
+    afterEach(() => {
+        window.localStorage.clear()
+    })
+
+    it('should return the stats for the current word length', () => {
+        const expectedState = mockStatsObject[`${wordLength}`]
+        const { result } = renderHook(() => useCurrentStats(wordLength))
+
+        expect(result.current.maxStreak).toEqual(expectedState.maxStreak)
+        expect(result.current.distribution).toStrictEqual(
+            expectedState.distribution,
+        )
+        expect(result.current.currentStreak).toEqual(
+            expectedState.currentStreak,
+        )
+    })
+
+    it('should set stats for the current word length', () => {
+        const newStats = {
+            maxStreak: 10,
+            currentStreak: 10,
+            distribution: {
+                '1': 0,
+                '2': 0,
+                '3': 0,
+                '4': 0,
+                '5': 0,
+                '6': 0,
+                loss: 0,
+            },
+        }
+        const { result } = renderHook(() => useCurrentStats(wordLength))
+
+        act(() => {
+            result.current.setCurrentStats(newStats)
+        })
+
+        expect(result.current.maxStreak).toEqual(10)
+        expect(result.current.distribution).toStrictEqual({
+            '1': 0,
+            '2': 0,
+            '3': 0,
+            '4': 0,
+            '5': 0,
+            '6': 0,
+            loss: 0,
+        })
+        expect(result.current.currentStreak).toEqual(10)
+    })
 })
