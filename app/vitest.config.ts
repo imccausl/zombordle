72738dcf117c280930c44884beba8ff711017944
Ian McCausland <imccausl@gmail.com>
2024-08-02 22:03:16 -0400

    chore: update sdks

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -7,10 +7,8 @@ import { defineConfig } from 'vitest/config'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
+    cacheDir: '../.vitest',
     test: {
-        cache: {
-            dir: '.vitest',
-        },
         environment: 'jsdom',
         globals: true,
         setupFiles: '../tests/setup.ts',
